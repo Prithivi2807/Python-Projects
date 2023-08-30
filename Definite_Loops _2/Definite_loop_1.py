090e9d61dbd0b25fcb6f3d676d07435908d0ee35
Prithivi2807 <prithivi2807@gamil.com>
2023-08-30 20:42:36 +0530

    loop def fun

---
+++ Definite_Loops _2/Definite_loop_1.py
@@ -0,0 +1,8 @@
+# Simple definite loop
+for i in [5, 4, 3, 2, 1]:
+    print(i)
+print()
+print('Definite Loops with Strings')
+friends = ['AK', 'Riyas', 'Prithivi', 'palani']
+for friend in friends:
+    print("Work together to become a success: ", friend)
