21b018dab9cbfc16cf879b7bf7f0e0af79747213
Prithivi2807 <prithivi2807@gamil.com>
2023-09-10 17:55:55 +0530

    lambda function practices

--- Chapter 1/Lambda_Function.py
+++ Chapter 1/Lambda_Function.py
@@ -1,4 +1,14 @@
-def x(a): return a*a
+x = lambda a: a*a
+print (x(4))
 
+add = lambda a,b : a+b 
+print(add(3,7))
 
-print(x(4))
+sub = lambda a,b : a-b
+print (sub(5,3))
+
+mul = lambda a,b : a*b 
+print (mul(66,33))
+
+div = lambda a,b : a/b
+print (div(2,2))
