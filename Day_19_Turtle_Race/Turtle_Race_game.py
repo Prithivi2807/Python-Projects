aaf0bc52a13c2785c7db3bf7eeecfe93aa76edce
Prithivi <prithivi2807@gmail.com>
2025-07-06 19:49:22 +0530

    Adding Turtle Race Project

--- Day_19_Turtle_Race/Turtle_Race_game.py
+++ Day_19_Turtle_Race/Turtle_Race_game.py
@@ -15,12 +15,11 @@ for turtle_index in range(0, 6):
     new_turtle.penup ()
     new_turtle.goto(x=-230, y=y_positions[turtle_index])
     all_turtles.append(new_turtle)
-
+    
 if user_bet:
     is_race_on = True
 
 while is_race_on:
-
     for turtle in all_turtles:
         if turtle.xcor() > 230:
             is_race_on = False
