ac691258313c37f1d9a747f8e47d4afafa8490af
Prithivi <prithivi2807@gmail.com>
2025-07-09 23:59:40 +0530

    Pong Arcade game

--- Day_20_Snake_game/food.py
+++ Day_20_Snake_game/food.py
@@ -1,12 +1,12 @@
 from turtle import Turtle
 import random
 
-class Food(Turtle):  # Superclas is a turtle class
+class Food(Turtle):  # Superclass is a turtle class
   def __init__(self):
     super().__init__()
     self.shape("circle")
     self.penup()
-    self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+    self.shapesize(stretch_len=0.7, stretch_wid=0.7)
     self.color("blue")
     self.speed("fastest")
     self.refresh()
